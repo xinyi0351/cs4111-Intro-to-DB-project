316f47604cdf0530699954699e01ba836046a11f
Max Bohn <mlb2249@columbia.edu>
2020-04-12 15:08:24 +0000

    added search function for artists

--- musicsearch-functions.py
+++ musicsearch-functions.py
@@ -98,5 +98,53 @@ def lookup():
         return render_template("index.html", **context)
 
 
+@app.route('/artview', methods = ['POST'])
+def artview():
+
+    #show list of all songs and albums by an artist
+    if request.method = 'POST':
+        artist = request.form('artist')
+
+        error = 0
+        if not artist:
+            error = 'Please enter an artist'
+
+        output = []
+
+        #get table of songs and corresponding albums by artist
+        cursor = g.conn.execute("SELECT song_name, album_name, song.published_date
+                                FROM artist, perform, song, contains, album
+                                WHERE artist.artist_id = perform.artist_id AND song.song_id = perform.song_id
+                                AND song.song_id = containst.song_id AND album.album_id = contains.album_id
+                                AND artist.artist_name = (%s)", artist)
+        
+        for result in cursor:
+            output.append(result[0], result[1], result[2])
+        cursor.close()
+        
+        if not output:
+            error = 'No results'
+
+        musicians = []
+
+        #get table of musicians who are members of the artist
+        cursor = g.conn.execute("SELECT musician_name, role, artist_name
+                                FROM artist AS ar, member_of AS m, musician AS mu
+                                WHERE ar.artist_id = m.artist_id AND m.musician_id = mu.musician_id
+                                AND ar.artist_name = (%s)", artist)
+
+        for result in cursor:
+            musicians.append(result[0], result[1], result[2])
+        cursor.close()
+        
+        error2 = 0
+        if not musicians:
+            error2 = 'No musicians listed'
+
+        context = dict(artistres = output, artistmsc = musicians)
+
+        return render_template("index.html", **context)
+
+
 
 
